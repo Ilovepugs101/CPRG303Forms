d8d7084bf090d25d1615d0f4c7032eb31f7c7ab1
Noah Zschogner <noahzschogner@gmail.com>
2025-11-20 20:11:03 -0700

    created files needed

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,20 @@
-import { Text, View } from "react-native";
+import { Text, View, Button, StyleSheet } from "react-native";
+import { router } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Button title="Employee Form" onPress={() => router.push("/employee")} />
+      <Button title="Employee Form" onPress={() => router.push("/employee")} />
+      <Button title="Employee Form" onPress={() => router.push("/employee")} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+})
